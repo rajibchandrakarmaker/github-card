41037b7c6675bf303fe009871f7e2f701f0bc8d5
Rajib karmaker <rajibchandrakarmaker@gmail.com>
2018-07-22 20:32:34 +0600

    github profile search - React js

---
+++ src/SearchProfile.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class SearchProfile extends Component{
+    render(){
+        return(
+            <div className="search--box">
+                <form onSubmit={this.handleForm.bind(this)}>
+                    <label><input type="search" ref="username" placeholder="Type Username + Enter"/></label>
+                </form>
+            </div>
+        )
+    }
+    handleForm(e){
+        e.preventDefault();
+        let username = this.refs.username.value;
+        this.props.fetchProfile(username);
+        this.refs.username.value= '';
+    }
+}
+
+export default SearchProfile
