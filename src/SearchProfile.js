5926d5133719a57d6017651cdbfa0a5bbd2c91df
Rajib karmaker <rajibchandrakarmaker@gmail.com>
2018-09-02 21:32:22 +0600

    bugs fixed - index module
    bugs fixed - index module

--- src/SearchProfile.js
+++ src/SearchProfile.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 
-
-class SearchProfile extends Component{
-    render(){
-        return(
-            <div className="search--box">
-                <form onSubmit={this.handleForm.bind(this)}>
-                    <label><input type="search" ref="username" placeholder="Type Username + Enter"/></label>
-                </form>
-            </div>
-        )
-    }
-    handleForm(e){
-        e.preventDefault();
-        let username = this.refs.username.value;
-        this.props.fetchProfile(username);
-        this.refs.username.value= '';
-    }
+class SearchProfile extends Component {
+  render() {
+    return (
+      <div className="search--box">
+        <form onSubmit={this.handleForm.bind(this)}>
+          <label>
+            <input
+              type="search"
+              ref="username"
+              placeholder="Enter your username"
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
+  handleForm(e) {
+    e.preventDefault();
+    let username = this.refs.username.value;
+    this.props.fetchProfile(username);
+    this.refs.username.value = '';
+  }
 }
 
-export default SearchProfile
+export default SearchProfile;
